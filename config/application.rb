3a9a126ea6abd6db31bf4ea1b15c21d69b0eac73
marcelh89 <marcelh89@googlemail.com>
2014-04-11 20:20:47 +0200

    agag

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,5 @@ module TadaEmber
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-	config.serve_static_assets = true
   end
 end
